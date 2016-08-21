48bfcec967b584841a41692dc5500a0e6469979f
Rob N ★ <robn@robn.io>
2016-08-21 14:15:40 +1000

    Stub out action commands
    Still not feeling right, but lets work towards something that I know
    will work and see if I get more of a feel for it.

--- src/execution.rs
+++ src/execution.rs
@@ -9,24 +9,64 @@ pub fn run(script: &Script, email: &MimeMessage) -> Vec<Action> {
 }
 
 fn run_commands(commands: &Vec<Command>, email: &MimeMessage) -> Vec<Action> {
-  commands.iter().flat_map(|c|
+  commands.iter().flat_map(|c| {
+    println!("{}", c.identifier);
     match &*c.identifier {
-      "require" => cmd_require(&c, email),
+
+      // controls
       "if"      => cmd_if(&c, email),
       "elsif"   => cmd_elsif(&c, email),
-      _         => vec!(), // XXX unknown command, throw exception
+      "else"    => cmd_else(&c, email),
+      "require" => cmd_require(&c, email),
+      "stop"    => cmd_stop(&c, email),
+
+      // actions
+      "fileinto" => cmd_fileinto(&c, email),
+      "redirect" => cmd_redirect(&c, email),
+      "keep"     => cmd_keep(&c, email),
+      "discard"  => cmd_discard(&c, email),
+
+      // XXX unknown command, throw exception
+      _         => vec!(),
     }
-  ).collect()
+  } ).collect()
+}
+
+fn cmd_if(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  run_commands(&command.commands, email);
+  vec!()
+}
+
+fn cmd_elsif(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  run_commands(&command.commands, email);
+  vec!()
+}
+
+fn cmd_else(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  run_commands(&command.commands, email);
+  vec!()
 }
 
 fn cmd_require(command: &Command, email: &MimeMessage) -> Vec<Action> {
   vec!()
 }
 
-fn cmd_if(command: &Command, email: &MimeMessage) -> Vec<Action> {
+fn cmd_stop(command: &Command, email: &MimeMessage) -> Vec<Action> {
   vec!()
 }
 
-fn cmd_elsif(command: &Command, email: &MimeMessage) -> Vec<Action> {
+fn cmd_fileinto(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  vec!()
+}
+
+fn cmd_redirect(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  vec!()
+}
+
+fn cmd_keep(command: &Command, email: &MimeMessage) -> Vec<Action> {
+  vec!()
+}
+
+fn cmd_discard(command: &Command, email: &MimeMessage) -> Vec<Action> {
   vec!()
 }
